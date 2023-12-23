570819d5710069b9efa612a12cb898e557ce7a47
Muhammad Ramadhan Putra Pratama <ramadhanputra1121@gmail.com>
2023-12-23 13:54:33 +0700

    initial code

---
+++ UASMobileComputing/app/src/main/java/com/example/uasmobilecomputing/Database/NoteRepository.kt
@@ -0,0 +1,21 @@
+package com.example.uasmobilecomputing.Database
+
+import androidx.lifecycle.LiveData
+import com.example.uasmobilecomputing.Models.Note
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    val allNotes : LiveData<List<Note>> = noteDao.getAllNotes()
+
+    suspend fun insert(note: Note){
+        noteDao.insert(note)
+    }
+
+    suspend fun delete(note: Note){
+        noteDao.delete(note)
+    }
+
+    suspend fun update(note: Note){
+        noteDao.update(note.id,note.title,note.note)
+    }
+}
---
+++ UASMobileComputing/app/src/main/java/com/example/uasmobilecomputing/utilities/contstants.kt
@@ -0,0 +1,3 @@
+package com.example.uasmobilecomputing.utilities
+
+const val DATABASE_NAME = "note_database"
